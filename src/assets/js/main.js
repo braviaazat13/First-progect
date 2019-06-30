28b1725337ddbfb5388f7400cde6e8111c74bc45
Azat <279733@gmail.com>
2019-06-30 17:41:50 +0200

    Add mega menu on jQuery

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,4 +7,20 @@ $(function() {
 	//@@include('global/_global.js')
 	//@@include('components/_components.js')
 	
+//Section: Drop Menu
+	$(document).ready(function() {
+		var $menuLiElement = $('.c-section--menu').find('.c-section__inner')
+			.children().children();
+		
+		$menuLiElement.mouseover(function() {
+			$(this).find('.c-drop-menu--main').addClass('h-flex');
+			
+		});
+		
+		$menuLiElement.mouseleave(function() {
+			$(this).find('.c-drop-menu--main').removeClass('h-flex');
+		});
+	});
+	// End of Section: Drop Menu
+	
 });
