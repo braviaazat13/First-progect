8837ac3a3f6255016a0036a022e1b866bd0e807b
Azat <279733@gmail.com>
2019-07-02 09:17:16 +0200

    Add drop menu in cart

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,23 +6,22 @@ $(function() {
 	
 	//@@include('global/_global.js')
 	//@@include('components/_components.js')
-	
+
 //Section: Drop Menu
 	$(document).ready(function() {
 		var $menuLiElement = $('[data-reletive=here]');
 		
 		$menuLiElement.mouseover(function() {
 			$(this).find('.c-drop-menu').addClass('h-flex');
-			
 		});
 		
 		$menuLiElement.mouseleave(function() {
 			$(this).find('.c-drop-menu').removeClass('h-flex');
 		});
+		
 	});
 	// End of Section: Drop Menu
 	
 });
-
 
 //$('.c-section--menu').find('.c-section__inner')
