7023ab8b94b144b805976ba915bdc0c3100e796d
Azat <279733@gmail.com>
2019-07-03 12:33:39 +0200

    fix product categories

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,19 +25,19 @@ $(function() {
 	//Section: Drop List
 	$(document).ready(function() {
 		var $parentElement = $('[data-reletive2=here]');
-		var $parentElement2 = $('[data-reletive3=here]');
 		
 		$parentElement.on('click', function() {
-			
-			$($parentElement2).children().children().removeClass('is-active');
 			
 			if ($(this).hasClass('is-active')) {
 				$(this).removeClass('is-active');
 				$(this).siblings('.c-section__list').removeClass('is-active');
 			}
+			
 			else {
-				$(this).toggleClass('is-active');
-				$(this).siblings('.c-section__list').toggleClass('is-active');
+				$($parentElement).removeClass('is-active');
+				$($parentElement).siblings('.c-section__list').removeClass('is-active');
+				$(this).addClass('is-active');
+				$(this).siblings('.c-section__list').addClass('is-active');
 			}
 		});
 	});
